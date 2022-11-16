c858fed430abe80416f7adc8df6ab91656598089
Tim Kopplin <timkopplin@hotmail.com>
2022-11-16 23:55:37 +0000

    parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -14,15 +14,16 @@ pool.connect()
 .catch(err => console.error('Error:', err));
 
 const cohortName = process.argv[2];
-
-pool.query(`
+const values = [cohortName]
+const queryString = `
 SELECT DISTINCT teachers.name, cohorts.name AS cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
-JOIN cohorts ON cohorts.name LIKE '${cohortName}'
+JOIN cohorts ON cohorts.name LIKE $1
 WHERE teachers.start_date < cohorts.start_date
 ORDER BY teachers.name;
-`)
+`
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
